05e75ffe8afa6b64ea519b3b1b4e0768a869a3c6
Pedro Durães <pedrofilipeduraes@hotmail.com>
2022-09-06 12:19:07 +0100

    add simple test for main page

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,13 +2,14 @@ import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+  it('renders main page', ()=> {
+    render(<Home />);
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+      name: 'Integer to Roman Numeral converter',
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
+    expect(heading).toBeInTheDocument();
+
+  });
 })
