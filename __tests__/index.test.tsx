d71057a684fa3bf8309a7ce04bb7c0e299c6ee46
Pedro Durães <pedrofilipeduraes@hotmail.com>
2022-09-07 12:09:17 +0100

    add tests related to main page user interaction

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Home from '@/pages/index'
 import { calculate } from '@/utils/index';
 import { CONVERT_TEST_CASES } from '@/mocks/index';
@@ -14,6 +14,37 @@ describe('Home', () => {
     expect(heading).toBeInTheDocument();
 
   });
+});
+
+describe('action tests', () => {
+  it('shows correct result in output section', ()=> {
+    render(<Home />);
+
+    const input = screen.getByPlaceholderText('Enter your integer here');
+    fireEvent.change(input, {target: {value: '4'}});
+    fireEvent.keyUp(input, {key: 'Enter', code: 'Enter', charCode: 13});
+    const outputElement = screen.getByText('IV');
+    expect(outputElement).toBeInTheDocument();
+
+  });
+
+  it('min value is 1', async ()=> {
+    render(<Home />);
+
+    const input = screen.getByPlaceholderText('Enter your integer here');
+    fireEvent.change(input, {target: {value: 0}});
+    expect(input).toHaveValue(1);
+  });
+
+  it('max value is 1000', async ()=> {
+    render(<Home />);
+
+    const input = screen.getByPlaceholderText('Enter your integer here');
+    fireEvent.change(input, {target: {value: 9999}});
+    expect(input).toHaveValue(1000);
+  });
+});
+
 describe('test convert logic', ()=> {
   it.each(CONVERT_TEST_CASES)('converts %s to %s', (input, expectedOutput)=> {
     expect(calculate(input)).toBe(expectedOutput);
